f0f401f5459a4e22d6d1f1ed054e299999472b85
Ranur-react <rahmatnur845@gmail.com>
2021-12-09 16:07:34 +0700

    Triger JOb, masih eror saat multple update employ job

--- DCL-09-12-2021-EXAMPLE TRIGER-JOB_HISTORY.sql
+++ DCL-09-12-2021-EXAMPLE TRIGER-JOB_HISTORY.sql
@@ -1,7 +1,6 @@
 UPDATE [dbo].[tb_m_employees] SET [dbo].[tb_m_employees].id_job='JOB03' 
 WHERE [dbo].[tb_m_employees].id='EMP01';
 
-
 SELECT * FROM [dbo].[tb_m_employees];
 
 DELETE FROM tb_tr_job_History;
